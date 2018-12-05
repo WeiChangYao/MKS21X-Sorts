aaab62441b5dae0df9a19c4e027e5dd222295617
weichanhyao <wyao1@stuy.edu>
2018-12-04 21:28:17 -0500

    started SelectionSort

---
+++ Sorts.java
@@ -0,0 +1,7 @@
+public class Sorts{
+  public static void selectionsort(int [] ary){ 
+    int [] newAry = new int [ary.length];
+    for (int i = 0; i < ary.length; i++){
+      for (int j = 0; j < ary.length; j++){
+        
+  
