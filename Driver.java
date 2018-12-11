25616d8654ddd136910533ff14345be2375fe8a1
weichangyao <wyao1@stuy.edu>
2018-12-11 10:52:54 -0500

    my insertionSort might be wrong

--- Driver.java
+++ Driver.java
@@ -12,13 +12,13 @@ public class Driver{
       Sorts.selectionSort(randish);
     }
     if(artie[1].equals("bubble")){
-      Sorts.bubbleSort(randish);
+      Sorts.insertionSort(randish);
     }
     if(artie[1].equals("test")){
       int[] randish2 = Arrays.copyOf(randish,randish.length);
       int[] randish3 = Arrays.copyOf(randish,randish.length);
       Sorts.selectionSort(randish);
-      Sorts.bubbleSort(randish2);
+      Sorts.insertionSort(randish2);
       Arrays.sort(randish3);
       if( Arrays.equals(randish,randish3)){
         System.out.println("Selection Correct!");
